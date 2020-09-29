6488acf73be5316649fe7fea586bc8bca28a9d28
Laura Logan <laurardh@aol.com>
2020-09-29 14:51:31 -0400

    debug touch event

--- main.js
+++ main.js
@@ -43,6 +43,7 @@ function toggleDatePicker (e) {
 }
 
 function toggleForMobile (e) {
+  e.stopPropagation();
   console.log('mobile touch event');
     dates_el.classList.toggle('active'); 
     selected_date_el.classList.toggle('purple'); 
@@ -169,12 +170,12 @@ function watchWindowSize() {
     console.log("i'm on a mobile device");
     date_picker_el.removeEventListener('click', toggleDatePicker);
     document.getElementsByTagName('body')[0].style.backgroundColor = 'blue';
-    // date_picker_el.addEventListener('touchstart', toggleForMobile);
-    date_picker_el.addEventListener('touchstart', function(e) {
-      for(let i=0; i< e.targetTouches.length; i++) {
-        console.log('touchpoint[' + i + '].target= ',  e.targetTouches[i].target);
-      }
-    })
+    date_picker_el.addEventListener('touchstart', toggleForMobile);
+    // date_picker_el.addEventListener('touchstart', function(e) {
+    //   for(let i=0; i< e.targetTouches.length; i++) {
+    //     console.log('touchpoint[' + i + '].target= ',  e.targetTouches[i].target);
+    //   }
+    // })
   } else {
     console.log("NOT on mobile");
     date_picker_el.addEventListener('click', toggleDatePicker);
